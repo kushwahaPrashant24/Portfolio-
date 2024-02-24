daf8bdadf9c85230fc4af2c68b2144376ae51126
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-02-24 23:29:46 +0530

    added Images in my Portfolio

--- src/Homepage/Content/Content.jsx
+++ src/Homepage/Content/Content.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import '../Content/Content.css';
-;
+import Image from '../../Images/png-removebg-preview.png'
+
+
 
 
 
 function Content (){
-    return <div>
-       fhgf
+    return <div id="content">
+       <div id="Images"><img src={Image}  /></div>
         
     </div>
 }
