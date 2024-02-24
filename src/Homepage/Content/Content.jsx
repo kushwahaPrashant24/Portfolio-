1f820419e2061bcd611981a2068393e98d533f17
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-02-24 23:58:44 +0530

    added styling  Images in my Portfolio

--- src/Homepage/Content/Content.jsx
+++ src/Homepage/Content/Content.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../Content/Content.css';
-import Image from '../../Images/png-removebg-preview.png'
+import Image from '../../Images/png-removebg-preview.png';
+import ScrollImg from '../../Images/scroll.png';
 
 
 
@@ -8,6 +9,16 @@ import Image from '../../Images/png-removebg-preview.png'
 
 function Content (){
     return <div id="content">
+    <div className="Wrapper">
+     <div id="text-area">
+        <h2>PRASHANT KUSHWAHA</h2>
+        <h1>Web Devloper</h1>
+        <div className="buttoms">See the Latest Work</div>
+        <div className="buttoms">Contact Me</div>
+       <img src={ScrollImg} />
+     </div>
+     </div>
+
        <div id="Images"><img src={Image}  /></div>
         
     </div>
