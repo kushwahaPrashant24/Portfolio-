7e311494fdd4deb9e7eeebdb3bc7df667382a080
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-02-25 17:44:36 +0530

    completed the Content section

--- src/Homepage/Content/textarea.jsx
+++ src/Homepage/Content/textarea.jsx
@@ -1,19 +1,54 @@
 import React from "react";
 import ScrollImg from '../../Images/scroll.png';
+import {motion} from "framer-motion";
+
+
+const textVariants = {
+   initial: {
+    x: -500,
+    opacity: 0,
+   },
+   animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+   },
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity
+      }
+    },
+
+};
+
+
 
 function textarea() {
-    return<div id="text-area">
-    <h2 id="text-h2">PRASHANT KUSHWAHA</h2>
-    <h1 id="text-h1">Web Developer</h1>
+    return<motion.div 
+    id="text-area" 
+    variants={textVariants}
+    initial="initial"
+    animate="animate"
+    >
+
+    <motion.h1 id="text-h1" variants={textVariants}>Web Developer</motion.h1>
 
 
-    <div id="text-info">
-    <div className="buttoms">See the Latest Work</div>
-    <div className="buttoms">Contact Me</div>
-    </div>
+    <motion.div id="text-info" variants={textVariants}>
+    <motion.div className="buttoms" variants={textVariants}>See the Latest Work</motion.div>
+    <motion.div className="buttoms" variants={textVariants}>Contact Me</motion.div>
+    </motion.div>
 
-   <img id="Scrolling" src={ScrollImg} />
- </div>
+   <motion.img id="Scrolling" src={ScrollImg} variants={textVariants}
+    animate="scrollButton"
+   />
+ </motion.div>
  
 }
 
