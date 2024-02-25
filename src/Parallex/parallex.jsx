8b6491b7100239c4c38b7e4b56b612471e4269a9
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-02-25 22:45:33 +0530

    completed the parallex effect

--- src/Parallex/parallex.jsx
+++ src/Parallex/parallex.jsx
@@ -1,13 +1,45 @@
 import React from "react";
+import { useRef } from "react";
 import "./parallex.css";
+import { motion, useScroll, useTransform } from "framer-motion";
+import ImagePlanet from "../Images/planets.png";
+import ImageSun from "../Images/sun.png";
 
 function Parallex({ type }) {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: [" start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
-    <div id="Parallex">
-      <h1 id="Parallex-h1"> {type === "services" ? "what we do?" : "what we did?"}</h1>
-      <div id="Mountains"></div>
-      <div id="Planets"></div>
-      <div id="Stars"></div>
+    <div
+      id="Parallex"
+      ref={ref}
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)",
+      }}
+    >
+      <motion.h1 id="Parallex-h1" style={{ y: yText }}>
+        {" "}
+        {type === "services" ? "what we do?" : "what we did?"}
+      </motion.h1>
+      <motion.div id="Mountains"></motion.div>
+
+      <motion.div
+        id="Planets"
+        style={{
+          y: yBg,
+        }}
+      ></motion.div>
+      <motion.div id="Stars" style={{ x: yBg }}></motion.div>
     </div>
   );
 }
