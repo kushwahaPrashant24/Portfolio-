a81a576518b991cee4a073ff98e32ad94dcfbf2a
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-04-18 09:58:42 +0530

    Started reconstruction of About section

---
+++ src/About me/About component/Abouttwo/Abouttwo.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Abouttwo() {
+  return (
+    <div id="Abouttwo">
+      <h1>ABOUT ME</h1>
+      <div id="AboutBorder"></div>
+
+      <h1>FRONT-END WEB DEVELOPER</h1>
+      <p>
+        A self-motivated, energetic, and passionate professional web Development
+        with valuable experience in React, MaterialUi, Frame Motion, SCSS. I
+        enjoy turning complex problems into simple, beautiful, and intuitive
+        interface designs
+      </p>
+
+      <div>
+        <form>
+          <label>Name: Prashant Kushwaha</label>
+
+          <label>Phone: +91 9795243902</label>
+
+          <label>
+            Education: Bechelors in Computer Science and Engineering
+          </label>
+
+          <label>
+            Address: RGS Nagar Boys Hostel, Avantika extension, Ghaziabad
+          </label>
+
+          <label>Email: prashantkush24@gmail.com</label>
+        </form>
+      </div>
+      <div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default Abouttwo;
